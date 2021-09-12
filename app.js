6be8994e8d7440662f150915624af7bf4daae179
Oleg <oleg17.k@gmail.com>
2021-09-12 17:40:49 +0300

    slider added

--- app.js
+++ app.js
@@ -1,40 +1,43 @@
-// modal 
+// modal
 
 let detailsBtn = document.querySelector(".contact-form");
 let modal = document.querySelector(".modal");
 
-
 function openModal() {
-    modal.classList.add("show");
-    modal.classList.remove("hide");
+  modal.classList.add("show");
+  modal.classList.remove("hide");
 }
 
 function closeModal() {
-    modal.classList.add("hide");
-    modal.classList.remove("show");
+  modal.classList.add("hide");
+  modal.classList.remove("show");
 }
 
-detailsBtn.addEventListener("click", function (e) {     
-    modal.classList.add("show");
-    modal.classList.remove("hide");    
+detailsBtn.addEventListener("click", function (e) {
+  modal.classList.add("show");
+  modal.classList.remove("hide");
 });
 
 modal.addEventListener("click", function (e) {
-    if (e.target === modal) {
-        closeModal();
-    }
+  if (e.target === modal) {
+    closeModal();
+  }
 });
 
-// mobile menu 
+// mobile menu
 
 let mobileMenu = document.querySelector(".nav-mobile-menu");
 let mainMenu = document.querySelector(".navigation");
 
 mobileMenu.addEventListener("click", function () {
-    mobileMenu.classList.toggle("active-menu");
-    if(mobileMenu.classList.contains("active-menu")) {
-        mainMenu.classList.add("active-menu");
-    } else {
-        mainMenu.classList.remove("active-menu");
-    }
+  mobileMenu.classList.toggle("active-menu");
+  if (mobileMenu.classList.contains("active-menu")) {
+    mainMenu.classList.add("active-menu");
+  } else {
+    mainMenu.classList.remove("active-menu");
+  }
 });
+
+// slider
+
+new Splide(".splide").mount();
